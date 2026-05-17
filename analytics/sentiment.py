561160d14e0ff891397891b6dfcfe0f664ffa751
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-17 11:24:03 +0530

    Add sentiment analysis function using TextBlob

--- analytics/sentiment.py
+++ analytics/sentiment.py
@@ -1,1 +1,14 @@
+from textblob import TextBlob
 
+def analyze_sentiment(text):
+
+    polarity = TextBlob(text).sentiment.polarity
+
+    if polarity > 0:
+        return "Positive"
+
+    elif polarity < 0:
+        return "Negative"
+
+    else:
+        return "Neutral"
