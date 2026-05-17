c68f868927eeb12423544c241a05421742cef751
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-17 11:40:23 +0530

    Implement issue detection in complaints review
    Added a function to detect various issues based on review content.

--- analytics/complaints.py
+++ analytics/complaints.py
@@ -1,1 +1,60 @@
+def detect_issue(review):
 
+    review = str(review).lower()
+
+    # LOGIN
+    if any(word in review for word in [
+        "login",
+        "otp",
+        "signin",
+        "sign in",
+        "password"
+    ]):
+        return "Login Issue"
+
+    # CRASH
+    elif any(word in review for word in [
+        "crash",
+        "hang",
+        "stuck",
+        "freeze",
+        "bug"
+    ]):
+        return "App Crash"
+
+    # SPEED
+    elif any(word in review for word in [
+        "slow",
+        "lag",
+        "loading",
+        "delay"
+    ]):
+        return "Performance Issue"
+
+    # UI
+    elif any(word in review for word in [
+        "ui",
+        "design",
+        "interface"
+    ]):
+        return "UI/UX Issue"
+
+    # TRANSACTION
+    elif any(word in review for word in [
+        "payment",
+        "transaction",
+        "withdraw",
+        "deposit"
+    ]):
+        return "Transaction Issue"
+
+    # SUPPORT
+    elif any(word in review for word in [
+        "support",
+        "service",
+        "customer care"
+    ]):
+        return "Support Issue"
+
+    else:
+        return "Other"
