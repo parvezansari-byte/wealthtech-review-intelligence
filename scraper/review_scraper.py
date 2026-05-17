d939109eb91259b4f4e880cf71e8138cac7422d8
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-17 11:22:09 +0530

    Add function to fetch reviews from Google Play

--- scraper/review_scraper.py
+++ scraper/review_scraper.py
@@ -1,1 +1,23 @@
+from google_play_scraper import reviews, Sort
+import pandas as pd
 
+def fetch_reviews(app_id, count=100):
+
+    result, _ = reviews(
+        app_id,
+        lang='en',
+        country='in',
+        sort=Sort.NEWEST,
+        count=count
+    )
+
+    data = []
+
+    for r in result:
+        data.append({
+            "review": r['content'],
+            "rating": r['score'],
+            "date": r['at']
+        })
+
+    return pd.DataFrame(data)
